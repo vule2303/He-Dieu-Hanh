e1f4ab3d4d5001d3649709f059cd48644065cb74
vule2303 <leductuanvu5@gmail.com>
2023-05-26 19:53:25 +0700

    done FIFO

---
+++ FIFO/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1;
+using System;
+
+
+
+public class Program {
+    static void Main(string[] args)
+    {
+        ThongTin a = new ThongTin();
+
+        var ListTT = new List<ThongTin>();
+
+        Console.Write("Nhap So Tien Trinh Muon Tao: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Ten TT {0}: ", i + 1);
+            string name = Console.ReadLine();
+            Console.Write("TG vao : ");
+            int tgVao = Int32.Parse(Console.ReadLine());
+            Console.Write("TG XL : ");
+            int tgXL = Int32.Parse(Console.ReadLine());
+
+            ThongTin TT = new ThongTin(name, tgVao, tgXL);
+
+            ListTT.Add(TT);
+
+        }
+        Console.WriteLine("==============CAC TIEN TRINH VUA NHAP ==============");
+        foreach (var TT in ListTT)
+        {
+            Console.WriteLine("{0} {1} {2}", TT.TenTT, TT.TDVao, TT.TGXuLy);
+        }
+        a.FIFO(ListTT);
+
+        
+        Console.ReadLine();
+
+
+    }
+
+   
+}
---
+++ FIFO/ConsoleApp1/ThongTin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ThongTin
+    {
+        public int TGXuLy { get; set; }
+        public int TDVao { get; set; }
+        public string TenTT { get; set; }
+
+        public ThongTin()
+        {
+        }
+
+        public ThongTin(string tenTT, int tGVao, int tGXuLy)
+        {
+            TenTT = tenTT;
+            TDVao = tGVao;
+            TGXuLy = tGXuLy;
+        }
+        public void FIFO(List<ThongTin> TT)
+        {
+            float tgTGCho = 0;
+            double tgTB;
+            var temp = 0;
+            Console.WriteLine("Thoi Gian Cho Cua Moi Tien Trinh");
+            foreach (var T in TT)
+            {
+                Console.Write("{0} = {1} ",T.TenTT, temp);
+                tgTGCho += temp - T.TDVao;
+
+                temp += T.TGXuLy;
+            }
+            tgTB = tgTGCho / TT.Count;
+            Console.WriteLine("\nThoi gian trung binh la: {0}", tgTB);
+        }
+        
+
+
+
+
+
+
+    }
+}
